73487afc073f1dd97bd2d042b47ab992be50ffdf
Unk <unknownow123@gmail.com>
2018-09-12 18:34:10 +0700

    Initial commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Transform player;
+    float moveX = 0, moveY = 0;
+    public float moveSpeed = 2f;
+
+	// Use this for initialization
+	void Awake () {
+        player = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        player.position += new Vector3(moveX * moveSpeed * Time.deltaTime, moveY * moveSpeed * Time.deltaTime,0);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public float health = 3f;
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+    }
+}
