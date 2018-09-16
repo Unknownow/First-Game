2682f78c4c01aecbd04c628da53ecf98c834b0b2
Unk <unknownow123@gmail.com>
2018-09-16 21:43:14 +0700

    Fix some name in ;aser boss and update laser boss

--- Assets/Bosses/Laser Boss/Scripts/Boss2Controller.cs
+++ Assets/Bosses/Laser Boss/Scripts/Boss2Controller.cs
@@ -2,111 +2,152 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss2Controller : MonoBehaviour {
+public class Boss2Controller : MonoBehaviour
+{
 
-	public GameObject boss2projectile;
-	public GameObject boss2laser;
+    public GameObject boss2projectile;
+    public GameObject boss2laser;
 
-	public Transform frontFace;
+    public Transform frontFace;
+    Transform laserSnipe;
 
-	float shootCooldown;
-	public float shootRate = 0.5f;
+    float shootCooldown;
+    public float shootRate = 1f;
 
-	float teleportCooldown;
-	public float teleportRate = 8f;
+    float teleportCooldown;
+    public float teleportRate = 4f;
 
-	float laserCooldown;
-	public float laserRate = 1f;
+    float laserCooldown;
+    public float laserRate = 1f;
 
-	bool isFreeze = false;
-	bool fired = false;
-	float freezeCooldown;
-	public float freezeRate = 1f;
+    bool isFreeze = false;
+    bool fired = false;
+    float freezeCooldown;
+    public float freezeRate = 1f;
 
-	public float safeTeleportDistance = 1f;
+    public float safeTeleportDistance = 1f;
 
-	public bool isPhase2 = false;
+    bool isPhase2 = false;
 
-	public Transform player;
-
-    BossManager boss;
+    public Transform player;
 
 
-	// Use this for initialization
-	void Start () {
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-		frontFace = transform.GetChild(0).transform;
-		teleportCooldown = teleportRate;
-		laserCooldown = laserRate;
-		freezeCooldown = freezeRate;
-        boss = gameObject.GetComponent<BossManager>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (!isPhase2 && boss.checkPhase2())
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        frontFace = transform.GetChild(0).transform;
+        laserSnipe = transform.GetChild(1);
+        teleportCooldown = teleportRate;
+        laserCooldown = laserRate;
+        freezeCooldown = freezeRate;
+        laserSnipe.GetComponent<Renderer>().enabled = false;
+    }
+
+    void Phase1()
+    {
+        isPhase2 = gameObject.GetComponent<BossManager>().checkPhase2();
+        if (isPhase2)
         {
-            isPhase2 = true;
+            laserSnipe.GetComponent<Renderer>().enabled = true;
+            shootRate /= 2;
+            teleportRate *= 2;
+            return;
+        }
+        Vector2 difference = player.position - transform.position;
+        float rotationDegreeToPlayer = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, -rotationDegreeToPlayer - transform.rotation.z);
+
+        if (teleportCooldown <= 0)
+        {
+            Vector2 telePos = Camera.main.ScreenToWorldPoint(new Vector2(Random.Range(0, Screen.width), Random.Range(0, Screen.height)));
+            float temp = Vector2.Distance(player.position, telePos);
+            if (temp >= safeTeleportDistance)
+            {
+                transform.position = telePos;
+                teleportCooldown = teleportRate;
+            }
+        }
+        else
+            teleportCooldown -= Time.deltaTime;
+
+        if (shootCooldown <= 0)
+        {
+            Quaternion ranRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, Random.Range(-180, 180));
+            Instantiate(boss2projectile, transform.position, ranRotation);
+            shootCooldown = shootRate;
+        }
+        else
+            shootCooldown -= Time.deltaTime;
+    }
+
+    void Phase2()
+    {
+        if (!isFreeze)
+        {
+            Vector2 difference = player.position - transform.position;
+            float rotationDegreeToPlayer = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, -rotationDegreeToPlayer - transform.rotation.z);
         }
 
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        if (teleportCooldown <= 0 && !isFreeze)
+        {
+            Vector2 telePos = Camera.main.ScreenToWorldPoint(new Vector2(Random.Range(0, Screen.width), Random.Range(0, Screen.height)));
+            float temp = Vector2.Distance(player.position, telePos);
+            if (temp >= safeTeleportDistance)
+            {
+                transform.position = telePos;
+                teleportCooldown = teleportRate;
+            }
+        }
+        else
+            teleportCooldown -= Time.deltaTime;
 
-		if (!isFreeze) 
-		{
-			Vector2 difference = player.position - transform.position;
-			float rotationDegreeToPlayer = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
-			transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, -rotationDegreeToPlayer - transform.rotation.z);
-		}
+        if (shootCooldown <= 0)
+        {
+            Quaternion ranRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, Random.Range(-180, 180));
+            Instantiate(boss2projectile, transform.position, ranRotation);
+            shootCooldown = shootRate;
+        }
+        else
+            shootCooldown -= Time.deltaTime;
 
-		if (teleportCooldown <= 0 && !isFreeze)
-		{
-			Vector2 telePos = Camera.main.ScreenToWorldPoint(new Vector2(Random.Range(0, Screen.width), Random.Range(0, Screen.height)));
-			float temp = Vector2.Distance(player.position, telePos);
-			if (temp >= safeTeleportDistance) 
-			{
-				transform.position = telePos;
-				teleportCooldown = teleportRate;
-			}
-		}
-		else
-			teleportCooldown -= Time.deltaTime;
-		
-		if (shootCooldown <= 0)
-		{
-			Quaternion ranRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, Random.Range(-180, 180));
-			Instantiate(boss2projectile, transform.position, ranRotation);
-			shootCooldown = shootRate;
-		}
-		else
-			shootCooldown -= Time.deltaTime;
+        if (laserCooldown <= 0)
+        {
+            isFreeze = true;
+            if (!fired)
+            {
+                if (freezeCooldown <= 0)
+                {
+                    Instantiate(boss2laser, frontFace.position, transform.rotation);
+                    fired = true;
+                    freezeCooldown = freezeRate;
+                    laserSnipe.GetComponent<Renderer>().enabled = false;
+                }
+                else
+                {
+                    laserSnipe.GetComponent<Renderer>().enabled = true;
+                    freezeCooldown -= Time.deltaTime;
+                }
+                    
+            }
+            else
+            {
+                isFreeze = false;
+                laserCooldown = laserRate;
+                fired = false;
+            }
+        }
+        else
+            laserCooldown -= Time.deltaTime;
+    }
 
-        
-
-		if (!isPhase2) 
-		{
-			if (laserCooldown <= 0) 
-			{
-				isFreeze = true;
-				if (!fired) 
-				{
-					if (freezeCooldown <= 0) 
-					{
-						Instantiate (boss2laser, frontFace.position, transform.rotation);
-						fired = true;
-						freezeCooldown = freezeRate;
-					}
-					else
-						freezeCooldown -= Time.deltaTime;
-				}
-				else 
-				{
-					isFreeze = false;
-					laserCooldown = laserRate;
-					fired = false;
-				}
-			} 
-			else 
-				laserCooldown -= Time.deltaTime;
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPhase2)
+            Phase1();
+        else
+            Phase2();
+    }
 }
--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -8,9 +8,19 @@ public class PlayerController : MonoBehaviour {
     float moveX = 0, moveY = 0;
     public float moveSpeed = 2f;
 
+    [Space]
+    [Header("Teleportation")]
+    public float teleCooldown = 2f;
+    public bool isTele = false;
+    float teleCooldownTimer;
+    public Vector3 target;
+
+
 	// Use this for initialization
 	void Awake () {
         player = GetComponent<Transform>();
+        teleCooldownTimer = teleCooldown;
+        isTele = false;
 	}
 	
 	// Update is called once per frame
@@ -25,6 +35,27 @@ public class PlayerController : MonoBehaviour {
         Vector2 difference = cursorPosition - transform.position;
         float rotationDegreeToCursor = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, -rotationDegreeToCursor - transform.rotation.z);
+        
+        if(isTele)
+        {
+            if(teleCooldownTimer == 0)
+            {
+                tele();
+            }
+            if (teleCooldownTimer < teleCooldown)
+                teleCooldownTimer += Time.deltaTime;
+            else
+            {
+                teleCooldownTimer = teleCooldown;
+                isTele = false;
+            }
+        }
+    }
+
+
+    void tele()
+    {
+        transform.position = target;
     }
 
 }
